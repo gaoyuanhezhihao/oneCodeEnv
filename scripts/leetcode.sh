9d78f2c04786f49e19a45cfc574f55459a91f894
Anna <gaoyuanhezhihao@139.com>
2017-07-24 18:07:20 +0800

    google test is added for leetcode

--- scripts/leetcode.sh
+++ scripts/leetcode.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 function leet(){
     echo "creating c++ project for leetcode..."
-    mkdir bin src build data include 
-    cp ~/.ycm_extra_conf.py ./
-    cp ~/oneCodeEnv/tmpl/leetcode/makefile.leetcode ./makefile
-    cp ~/oneCodeEnv/tmpl/leetcode/main.cpp ./src/main.cpp
-    cp ~/oneCodeEnv/tmpl/leetcode/solution.hpp ./include/solution.hpp
+    mkdir -p bin src build data include 
+    cp -n ~/.ycm_extra_conf.py ./
+    cp -n ~/oneCodeEnv/tmpl/leetcode/makefile.leetcode ./makefile
+    cp -n ~/oneCodeEnv/tmpl/leetcode/main.cpp ./src/main.cpp
+    cp -n ~/oneCodeEnv/tmpl/leetcode/solution.hpp ./include/solution.hpp
+    cp -n ~/oneCodeEnv/tmpl/leetcode/run.sh  ./run.sh
 
-    cp -r ~/oneCodeEnv/tmpl/leetcode/test ./
+    cp -nr ~/oneCodeEnv/tmpl/leetcode/test ./
+    ln -s ~/oneCodeEnv/tmpl/googleTest/gtest-all.o ./test/gtest-all.o
+    ln -s ~/oneCodeEnv/tmpl/googleTest/gtest_main.a ./test/gtest_main.a
+    ln -s ~/oneCodeEnv/tmpl/googleTest/gtest_main.o ./test/gtest_main.o
 }
