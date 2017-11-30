adb4789cd666f5fb1fcbf6ee473a2dc39b9cbdbe
Anna <gaoyuanhezhihao@139.com>
2017-11-30 15:15:49 +0800

    map remote jupyter server port with arg of server ip

--- shell/func/jupy_tesla.sh
+++ shell/func/jupy_tesla.sh
@@ -1,4 +1,5 @@
 function jupy(){
     echo "connect jupyter server..."
-    source ~/oneCodeEnv/network/jupy_tesla.sh
+    #source ~/oneCodeEnv/network/jupy_tesla.sh
+    ssh -N -f -L 127.0.0.1:8889:127.0.0.1:8889 hzh@$*
 }
