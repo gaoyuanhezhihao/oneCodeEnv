d9bf2f3e3aa279f05a536970ec23cf5cd56eebe3
Anna <gaoyuanhezhihao@139.com>
2017-12-18 10:16:22 +0800

    untrack vim/bundle from now on.

--- install/vim.sh
+++ install/vim.sh
@@ -8,5 +8,7 @@ cd src
 ./configure --with-features=huge --enable-gui=gnome2 --enable-luainterp=yes --enable-pythoninterp=yes --enable-perlinterp=yes --prefix=$DIR
 make
 make install
+git clone https://github.com/VundleVim/Vundle.vim.git ~/oneCodeEnv/vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe/
 python2 ./install.py --clang-completer
