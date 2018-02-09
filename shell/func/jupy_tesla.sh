b4d49eef14fbbc0de50a77824c89d65af6be8ed0
Anna <gaoyuanhezhihao@139.com>
2018-02-09 10:31:14 +0800

    map remote jupy server accept parameters

--- shell/func/jupy_tesla.sh
+++ shell/func/jupy_tesla.sh
@@ -1,5 +1,31 @@
 function jupy(){
     echo "connect jupyter server..."
+    IP="" 
+    Remote_Port=8889
+    This_Port=8889
+
+    case $# in
+        3)
+            #echo 3
+            IP=$1
+            Remote_Port=$2
+            This_Port=$3
+            ;;
+        2)
+            #echo 2
+            IP=$1
+            Remote_Port=$2
+            ;;
+        1)
+            #echo 1
+            IP=$1
+            ;;
+        *)
+            echo "wrong format of parameters.\nexample: jupy 114.214.123.23 8889 8558"
+            return 1
+            ;;
+    esac
     #source ~/oneCodeEnv/network/jupy_tesla.sh
-    ssh -N -f -L 127.0.0.1:8889:127.0.0.1:8889 hzh@$*
+    echo $IP":"$Remote_Port "--> 127.0.0.1:"$This_Port
+    #echo ssh -N -f -L 127.0.0.1:$This_Port:127.0.0.1:$Remote_Port hzh@$IP
 }
