fc03b721c9ed10561462b94931bafae6883235f6
zhihaohe <zhihaohe@autox.ai>
2022-05-05 20:00:44 +0800

    +Download video of webpage

---
+++ script/Download_video_of_webpage.py
@@ -0,0 +1,11 @@
+import re
+
+with open("./a.html", 'r') as f:
+    lines = f.readlines()
+
+for l in lines:
+    result = re.search(r'http:.*\.mp4', l)
+    if result:
+        print(result.group(0))
+        # for g in result.group():
+            # print(g)
